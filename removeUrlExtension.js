53d25c8f0ffb57d803ccefccf51c9622c4fc8e5e
Samotnikv1 <82444700+Samotnikv1@users.noreply.github.com>
2021-05-05 16:13:55 +0200

    Update removeUrlExtension.js

--- removeUrlExtension.js
+++ removeUrlExtension.js
@@ -1,4 +1,4 @@
 var url = window.location.href;
-url = url.split('.html')[0];
+url = url.split('')[0];
 //url = url.substring(0, url.lastIndexOf("."));
 window.history.replaceState( null, null, url );
