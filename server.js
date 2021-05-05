4f517eaaddefc000a79498e3d408140b0bc19701
Samotnikv1 <82444700+Samotnikv1@users.noreply.github.com>
2021-05-05 16:10:36 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -7,21 +7,6 @@ app.use(express.static("public"));
 
 
 
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/index.html");
-});
-
-app.get("/index.html", (request, response) => {
-  response.sendFile(__dirname + "/index.html");
-});
-
-app.get("/COMMANDS.html", (request, response) => {
-  response.sendFile(__dirname);
-});
-
-app.get("*", (request, response) => {
-  response.sendFile(__dirname + "/404.html");
-});
 
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
